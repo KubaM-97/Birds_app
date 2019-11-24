c5ff58faed40ea5cd0950541266594e1e0abab10
KubaM-97 <jakubm16@interia.pl>
2019-11-25 00:04:42 +0100

    project

--- script.js
+++ script.js
@@ -1,53 +1,105 @@
 $(function () {
-  
+
+    //calendar
     $("#datepicker").datepicker({
         'autoclose': true,
         'format': 'dd-mm-yyyy'
     });
- 
 
-    
+
+    //change background photo
     let $nr = 0;
-            let $background_Image = ["sowa.jpg", "orly.jpg", "bialorzytki.jpg", "czapla.jpg"];
+    let $background_Image = ["sowa.jpg", "orly.jpg", "bialorzytki.jpg", "czapla.jpg"];
 
-            setInterval(function() {
-                //photo's change
-                $nr++;
-                if ($nr >= $background_Image.length) $nr = 0;
-                $(".layer").animate({
-                    opacity: 1
-                });
+    setInterval(function () {
 
-                let $file = `photos/Main-images/${$background_Image[$nr]}`;
+        $nr++;
+        if ($nr >= $background_Image.length) $nr = 0;
+        $(".layer").animate({
+            opacity: 1
+        });
+
+        let $file = `photos/Main-images/${$background_Image[$nr]}`;
+        
+        $(".layer").animate({}, 2000, function () {
+            $(this).fadeTo(7000, 0.75, function () {
+                $(this).css('background-image', 'url(' + $file + ')');
+            }).fadeTo(7000, 1);
+
+        });
+
+    }, 8000);
 
 
-                
-//
-//                $(".layer").animate({}, 2000, function() {
-//                    $(this).fadeTo(3000, 0.7, function()
-//                        {
-//                            $(this).css('background-image', 'url(' + $file + ')');
-//                        }).fadeTo(1000, 1);
-//                    
-//                });
-//
-//            }, 3000);
+
+
     
-             
-           
-        $("input:checkbox").click(function(){
-               if(!($(this).is(":checked"))){
-                   $(this).closest(".container__content__ans--colour").removeClass("checked");
-               }
-               else{
-                   
-               $(this).closest(".container__content__ans--colour").addClass("checked");
-               }
-           }); 
+    //another step
+    $('.container[id!="1"]').hide();
+
+
+    $("input:button").click(function () {
+        let $id = $(this).closest(".container").attr("id");
+        $(this).closest(".container").hide(1000);
+        $id++;
+        $(".container#" + $id).delay(1000).fadeTo(1000, 1);
+    });
+
+    $(".arrow-right").click(function () {
+        let $id = $(this).closest(".container").attr("id");
+        $(this).closest(".container").hide(1000);
+        $id++;
+        $(".container#" + $id).delay(1000).fadeTo(1000, 1);
+    });
     
-        $("input:radio").click(function() {
-                $(".container__content__ans--bucks").removeClass("checked");
-                $(this).closest(".container__content__ans--bucks").addClass("checked");
-            });
-});
+    $(".arrow-left").click(function () {
+        let $id = $(this).closest(".container").attr("id");
+        $(this).closest(".container").hide(1000);
+        $id--;
+        $(".container#" + $id).delay(1000).fadeTo(1000, 1);
+    });
+
+
+
+
+
+
+
+    //inputs
+
+    $("input:checkbox").click(function () {
+        if (!($(this).is(":checked"))) {
+            $(this).closest(".container__content__ans").removeClass("checked");
+        } else {
+
+            $(this).closest(".container__content__ans").addClass("checked");
+        }
+    });
+
+    $("input:radio").click(function () {
+        $(".container__content__ans").removeClass("checked");
+        $(this).closest(".container__content__ans").addClass("checked");
+    });
+    
+    
+    
+    
+    //JSON
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            $(".container__results").html(xhr.responseText);
+        }
+    };
+    xhr.open("GET", "results.json", true);
+    xhr.send(null);
+    
+//    let myRequest = new Request("./result.json")
+//        .then(function(resp){
+//            return resp.json()
+//        })
+//        .then(function(){
+//            alert("Hello");
+//        })
+    
 });
